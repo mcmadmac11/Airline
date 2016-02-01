e5e4d8bd9b496e954f7920250f6193c15f8d222d
Dan McManus <mcmanusdbm@gmail.com>
2016-02-01 17:03:20 -0600

    recovered file THANKS GITHUBgit pull!

---
+++ AirlineRepo/AirlineLibrary/SeatChart.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirlineLibrary
+{
+    class SeatChart
+    {
+        public Seat[,] SeatConfig = new Seat[10, 3];
+        enum seatLetter { A, B, C, D, E, F, G, H, I, J };
+        public SeatChart()
+        {
+        }
+        public void populateSeatChart()
+        {
+
+
+            for (int seatLetter = 0; seatLetter < 10; seatLetter++)
+            {
+                for (int insideArrayCounter = 0; insideArrayCounter < 3; insideArrayCounter++)
+                {
+                    var x = SeatConfig[seatLetter, insideArrayCounter];
+                }
+            }
+        }
+
+    }
+}
