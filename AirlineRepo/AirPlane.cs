60c802a489aa460746da646cc28aa8cd0b04a6c1
Adam <adamhool414@gmail.com>
2016-02-01 14:35:03 -0600

    outline of classes

---
+++ AirlineRepo/AirPlane.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Airline
+{
+    class AirPlane
+    {
+        int PassengerCapacity;
+        int Range;
+        bool MaintenanceStatus;
+        string Schedule;
+        public AirPlane(int passengerCapacity, int range, bool maintenanceStatus, string schedule)
+        {
+            this.PassengerCapacity = passengerCapacity;
+            this.Range = range;
+            this.MaintenanceStatus = maintenanceStatus;
+            this.Schedule = schedule;
+        }
+    }
+}
---
+++ AirlineRepo/Flight.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Airline
+{
+    class Flight
+    {
+        AirPlane airPlane;
+        int FlightNumber;
+        Origin origin;
+        Destination destination;
+        List<Passenger> PassengerManifest;
+        public Flight(AirPlane airPlane, int flightNumber, Origin origin, Destination destination, List<Passenger>passengerManifest)
+        {
+            this.airPlane = airPlane;
+            this.FlightNumber = flightNumber;
+            this.origin = origin;
+            this.destination = destination;
+            this.PassengerManifest = passengerManifest;
+        }
+    }
+}
