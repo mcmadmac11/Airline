6e6e473ccb9cbe8bc604ef274e3b62cab90fe215
Adam <adamhool414@gmail.com>
2016-02-02 10:41:46 -0600

    seatchart works

--- AirlineRepo/AirlineLibrary/SeatChart.cs
+++ AirlineRepo/AirlineLibrary/SeatChart.cs
@@ -8,19 +8,36 @@ namespace AirlineLibrary
 {
     public class SeatChart
     {
-        public Seat[,] SeatConfig = new Seat[10, 3];
-        enum seatLetter { A, B, C, D, E, F, G, H, I, J };
+        public Seat[,] SeatConfig;
+        public string[] seatNameLeter;
+        public string[] seatNameNumber;
+        public int[] seatPrice;
         public SeatChart()
         {
-            
+            SeatConfig = new Seat[10, 3];
+            seatNameLeter = new string[10] { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J" };
+            seatNameNumber = new string[3] { "1", "2", "3" };
+            seatPrice = new int[3] { 100, 200, 300 };
         }
+        //public void populateSeatNameLetter()
+        //{
+        //    for (int counter = 0; counter < 10; counter++)
+        //    {
+        //        seatNameLeter[counter]=
+        //    }
+        //}
+
+
         public void populateSeatChart()
         {
-            for(int seatLetter = 0; seatLetter<10; seatLetter++)
+
+
+            for(int outsideArrayCounter = 0; outsideArrayCounter < 10; outsideArrayCounter++)
             {
-                for(int insideArrayCounter = 0; insideArrayCounter<3; insideArrayCounter++)
+                for (int insideArrayCounter = 0; insideArrayCounter < 3; insideArrayCounter++)
                 {
-                    var x = SeatConfig[seatLetter, insideArrayCounter];
+                    SeatConfig[outsideArrayCounter, insideArrayCounter] = new Seat (seatNameLeter[outsideArrayCounter]+seatNameNumber[insideArrayCounter], seatPrice[insideArrayCounter]);
+                    Console.WriteLine("{0} {1}",seatNameLeter[outsideArrayCounter], seatNameNumber[insideArrayCounter]);
                 }
             }
         } 
